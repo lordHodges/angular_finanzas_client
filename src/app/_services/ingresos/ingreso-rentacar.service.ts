7611bb85c8c59aa1d03d0d5fef067c7bda646df9
victor <hodgestroncoso@gmail.com>
2020-11-20 12:10:02 -0300

    porbando recuperacion

---
+++ src/app/_services/ingresos/ingreso-rentacar.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+@Injectable({
+  providedIn: 'root',
+})
+export class IngresoRentacarService {
+  constructor(private http: HttpClient, private router: Router) {}
+  getArriendos() {
+    return this.http.get<[]>(`${environment.apiUrl}/rentacar/arriendos`);
+  }
+}
